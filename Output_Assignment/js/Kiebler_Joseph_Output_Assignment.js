033431feb0311a6855f041041930626e42335fc8
Joseph Kiebler <jkiebler@fullsail.edu>
2015-09-29 23:26:38 -0500

    Output Assignment -- Commit 2
    Created variables; comments for variables;

---
+++ Output_Assignment/js/Kiebler_Joseph_Output_Assignment.js
@@ -0,0 +1,8 @@
+//Define variables
+
+//String Variable; Define type of car driven
+var myCar = "2006 Nissan Maxima";
+//Number Variable; Define length of time lived in this area
+var livedHere = 22;
+//Boolean Variable; Define validity of love for music
+var loveMusic = true;
