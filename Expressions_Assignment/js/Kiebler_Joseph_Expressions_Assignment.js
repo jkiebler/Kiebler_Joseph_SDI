a2a1cc046ebd105754c00247310b95bff6cf40f9
Joseph Kiebler <jkiebler@fullsail.edu>
2015-10-08 02:40:16 -0500

    Expressions Assignment -- Commit 4
    Added instructions/explanation of tool;

--- Expressions_Assignment/js/Kiebler_Joseph_Expressions_Assignment.js
+++ Expressions_Assignment/js/Kiebler_Joseph_Expressions_Assignment.js
@@ -8,3 +8,8 @@ Assignment: Expressions Assignment */
 var payRate;  //global variable for rate of pay
 var hoursWorked; //global variable for hours worked
 var netPay;  //global variable for net pay
+
+//Main Code
+console.log("Welcome to my net pay calculator. I created this calculator to help me estimate what I would bring home on my paycheck. " +
+            "This paycheck calculator assumes the data being entered is for a person in the state of Kentucky who claims no exceptions " +
+            "on their W-4 and whose income falls in the slightly above minimum wage tax bracket.");  //Introduction for tool
