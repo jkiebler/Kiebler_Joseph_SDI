e2a530a1eab5d5fff1e0376df8523b5102cbf9ab
Joseph Kiebler <jkiebler@fullsail.edu>
2015-10-08 02:40:58 -0500

    Expressions Assignment -- Commit 5
    Set variable values; did some math

--- Expressions_Assignment/js/Kiebler_Joseph_Expressions_Assignment.js
+++ Expressions_Assignment/js/Kiebler_Joseph_Expressions_Assignment.js
@@ -13,3 +13,10 @@ var netPay;  //global variable for net pay
 console.log("Welcome to my net pay calculator. I created this calculator to help me estimate what I would bring home on my paycheck. " +
             "This paycheck calculator assumes the data being entered is for a person in the state of Kentucky who claims no exceptions " +
             "on their W-4 and whose income falls in the slightly above minimum wage tax bracket.");  //Introduction for tool
+payRate = prompt("What is your pay rate?");  //set value of payRate variable to what is entered in prompt
+Number(payRate);  //convert payRate from string to number
+
+hoursWorked = prompt("How many hours will be on this check?");  //set value of hoursWorked to value of prompt
+Number(hoursWorked);
+
+netPay = (payRate * hoursWorked) * 0.24;  //set netPay to result of equation
