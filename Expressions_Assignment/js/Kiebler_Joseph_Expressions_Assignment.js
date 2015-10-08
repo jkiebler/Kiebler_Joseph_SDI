cc88c44043734e727a54e833164460a9ed311bc2
Joseph Kiebler <jkiebler@fullsail.edu>
2015-10-08 02:49:55 -0500

    Expressions Assignment -- Commit 7
    Added a couple more variables to make the math accurate; fixed
    equations;

--- Expressions_Assignment/js/Kiebler_Joseph_Expressions_Assignment.js
+++ Expressions_Assignment/js/Kiebler_Joseph_Expressions_Assignment.js
@@ -7,18 +7,28 @@ Assignment: Expressions Assignment */
 
 var payRate;  //global variable for rate of pay
 var hoursWorked; //global variable for hours worked
+var taxRate; //global variable for tax rate
+var grossPay; //global variable for gross pay
+var taxPaid; //global variable for tax paid
 var netPay;  //global variable for net pay
 
 //Main Code
-console.log("Welcome to my net pay calculator. I created this calculator to help me estimate what I would bring home on my paycheck. " +
+alert("Welcome to my net pay calculator. I created this calculator to help me estimate what I would bring home on my paycheck. " +
             "This paycheck calculator assumes the data being entered is for a person in the state of Kentucky who claims no exceptions " +
             "on their W-4 and whose income falls in the slightly above minimum wage tax bracket.");  //Introduction for tool
 payRate = prompt("What is your pay rate?");  //set value of payRate variable to what is entered in prompt
 Number(payRate);  //convert payRate from string to number
 
 hoursWorked = prompt("How many hours will be on this check?");  //set value of hoursWorked to value of prompt
-Number(hoursWorked);
+Number(hoursWorked); //convert hoursWorked from string to number
 
-netPay = (payRate * hoursWorked) * 0.24;  //set netPay to result of equation
+taxRate = prompt("What rate is your pay taxed at?"); //set value of taxRate to prompt
+Number(taxRate);  //convert taxRate from string to number
+
+grossPay = (payRate * hoursWorked); //set grossPay to value of calculation
+
+taxPaid = grossPay * (taxRate / 100);  //set taxPaid to result of equation
+
+netPay = grossPay - taxPaid; //set netPay to result of calculation
 
 console.log("You can expect $" + netPay + " on your next check.");  //display estimated value of paycheck
