37bbdeaa2660b7fb5651100f2c5c4163ef51b074
Joseph Kiebler <jkiebler@fullsail.edu>
2015-10-08 02:59:04 -0500

    Expressions Worksheet -- Commit 1
    Set up files and completed first problem;

---
+++ Expressions_Worksheet/js/Kiebler_Joseph_Expressions_Assignment.js
@@ -0,0 +1,15 @@
+/* Name: Joseph Kiebler
+Class: SDI Section 01
+Date: 10/08/2015
+Assignment: Expressions Worksheet */
+
+//Problem 1: Sparky
+
+    //Variables
+    var humanYears = 7;  //define Sparky's age in human years
+    var dogYears;  //create empty variable for Sparky's age in dog years
+
+    //Main Code
+    dogYears = humanYears * 7;  //set dogYears to humanYears times 7
+    
+    console.log("Sparky is " + humanYears + " years old in human years, which is " + dogYears + " years old in dog years.";  //print result to console   
