2725579037bebd534d6d6392d4f6b896802000a8
Joseph Kiebler <jkiebler@fullsail.edu>
2015-10-08 02:39:11 -0500

    Expressions Assignment -- Commit 3
    Defined variables;

--- Expressions_Assignment/js/Kiebler_Joseph_Expressions_Assignment.js
+++ Expressions_Assignment/js/Kiebler_Joseph_Expressions_Assignment.js
@@ -2,3 +2,9 @@
 Class: SDI Section 01
 Date: 10/08/2015
 Assignment: Expressions Assignment */
+
+//Variables
+
+var payRate;  //global variable for rate of pay
+var hoursWorked; //global variable for hours worked
+var netPay;  //global variable for net pay
