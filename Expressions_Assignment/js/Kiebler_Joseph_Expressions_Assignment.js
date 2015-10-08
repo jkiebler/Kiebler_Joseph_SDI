6ec0e25e28557c7e110bfa14de7a7ffc86b8f1f9
Joseph Kiebler <jkiebler@fullsail.edu>
2015-10-08 13:32:00 -0500

    Expressions Assignment -- Commit 8
    Added array; added alert stating where to find result of calculation;

--- Expressions_Assignment/js/Kiebler_Joseph_Expressions_Assignment.js
+++ Expressions_Assignment/js/Kiebler_Joseph_Expressions_Assignment.js
@@ -11,6 +11,7 @@ var taxRate; //global variable for tax rate
 var grossPay; //global variable for gross pay
 var taxPaid; //global variable for tax paid
 var netPay;  //global variable for net pay
+var storageVar = [0, 1, 2, 3, 4]; //global variable to store data
 
 //Main Code
 alert("Welcome to my net pay calculator. I created this calculator to help me estimate what I would bring home on my paycheck. " +
@@ -18,17 +19,24 @@ alert("Welcome to my net pay calculator. I created this calculator to help me es
             "on their W-4 and whose income falls in the slightly above minimum wage tax bracket.");  //Introduction for tool
 payRate = prompt("What is your pay rate?");  //set value of payRate variable to what is entered in prompt
 Number(payRate);  //convert payRate from string to number
+storageVar[0] = payRate;  //set first value of storageVar to result
 
 hoursWorked = prompt("How many hours will be on this check?");  //set value of hoursWorked to value of prompt
 Number(hoursWorked); //convert hoursWorked from string to number
+storageVar[1] = hoursWorked;  //set second value of storageVar to result
 
 taxRate = prompt("What rate is your pay taxed at?"); //set value of taxRate to prompt
 Number(taxRate);  //convert taxRate from string to number
+storageVar[2] = taxRate;  //set third value of storageVar to result;
 
-grossPay = (payRate * hoursWorked); //set grossPay to value of calculation
+grossPay = (storageVar[0] * storageVar[1]); //set grossPay to value of calculation
+storageVar[3] = grossPay;  //store grossPay as third value of storageVar array
 
-taxPaid = grossPay * (taxRate / 100);  //set taxPaid to result of equation
+taxPaid = storageVar[3] * (storageVar[2] / 100);  //set taxPaid to value of calculation
+storageVar[4] = taxPaid;  //set the final value in the array to taxPaid
 
-netPay = grossPay - taxPaid; //set netPay to result of calculation
+netPay = storageVar[3] - storageVar[4]; //set netPay to result of calculation
+
+alert("Calculation done! Please see the console for your result!");  //alert instructing where to find result
 
 console.log("You can expect $" + netPay + " on your next check.");  //display estimated value of paycheck
