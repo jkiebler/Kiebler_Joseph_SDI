3464e5a7788cbe2a21c62fdfbf90bf928164dd83
Joseph Kiebler <jkiebler@fullsail.edu>
2015-10-08 03:05:05 -0500

    Expressions Worksheet -- Commit 2
    Completed second problem;

--- Expressions_Worksheet/js/Kiebler_Joseph_Expressions_Assignment.js
+++ Expressions_Worksheet/js/Kiebler_Joseph_Expressions_Assignment.js
@@ -13,3 +13,19 @@ Assignment: Expressions Worksheet */
     dogYears = humanYears * 7;  //set dogYears to humanYears times 7
     
     console.log("Sparky is " + humanYears + " years old in human years, which is " + dogYears + " years old in dog years.";  //print result to console   
+                
+
+
+
+//Problem 2: Slice of Pie Part 1
+                
+    //Variables
+    var numberPizzas = 10;  //variable for total number of pizzas ordered;
+    var numberPeople = 15;  //variable for number of people
+    var slicesPerPizza = 8;  //variable for number of slices per pizza
+    var numberSlices = numberPizzas * slicesPerPizza;  //variable to determine total number of slices
+    var numberPerPerson;
+                
+    //Main Code
+    numberPerPerson = numberSlices / numberPeople;  //set value of numberPerPerson to result of equation
+    console.log("Each person ate " + numberPerPerson + " slices of pizza at the party.";  //display result in console
